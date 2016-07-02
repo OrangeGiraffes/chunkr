d37f0efa6ef27db2be25865ba2039c0cce024419
joshuawalcher <joshuawalcher@yahoo.com>
2016-07-02 09:28:17 -0500

    Updates after testing initial entry to app

--- includes/functions.php
+++ includes/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-function GetConfig ($variable)
+function GetConfig($variable)
 {
     if (isset($GLOBALS['APP_CFG'][$variable]) && $GLOBALS['APP_CFG'][$variable] != '') {
             return $GLOBALS['APP_CFG'][$variable];
@@ -8,7 +8,7 @@ function GetConfig ($variable)
     return false;
 }
 
-function CheckLoginStatus ()
+function CheckLoginStatus()
 {
     if (isset($_SESSION['user_logged_in']) && $_SESSION['user_logged_in'] == GetCookie('chunkr')) { 
         return true;
@@ -16,20 +16,24 @@ function CheckLoginStatus ()
     return false;
 }
 
-function Parse ($templateFile)
+function Parse($templateFile)
 {
-    return ShowHeader() . preg_replace("/\{{([^\{]{1,100}?)\}}/e","\$GLOBALS['$1']",file_get_contents(APP_TEMPLATE_PATH . $templateFile . '.html')) . ShowFooter();
+    die(
+    ShowHeader() .
+        file_get_contents(APP_TEMPLATE_PATH . $templateFile . '.html') .
+    ShowFooter()
+    );
 }
 
 function ShowHeader(){
-    return preg_replace("/\{{([^\{]{1,100}?)\}}/e","\$GLOBALS['$1']",file_get_contents(APP_TEMPLATE_PATH . 'header.html'));
+    return file_get_contents(APP_TEMPLATE_PATH . 'header.html');
 }
 
 function ShowFooter(){
-    return preg_replace("/\{{([^\{]{1,100}?)\}}/e","\$GLOBALS['$1']",file_get_contents(APP_TEMPLATE_PATH . 'footer.html'));
+    return file_get_contents(APP_TEMPLATE_PATH . 'footer.html');
 }
 
-function GetCookie ($cookieName)
+function GetCookie($cookieName)
 {
     if (!isset($_COOKIE[$cookieName])) {
         return '';
@@ -38,7 +42,7 @@ function GetCookie ($cookieName)
     }
 }
 
-function ThrowError ($message)
+function ThrowError($message)
 {
     die('failure|' . $message);    
 }
